11e99beb0fce38f60312008e147a793c5a4769a2
Enrico Marino <enrico.marino@email.com>
2013-08-20 19:15:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-module.export = function () {};
+module.exports = function (array, iterator, callback) {
+  var completed = 0;
+  var len = array.length;
+  var complete;
+  var i;
+
+  complete = function (err, result) {
+    if (err) {
+      callback(err);
+      callback = function () {};
+      return;
+    }
+    if (!result) {
+      callback(err, false);
+      callback = function () {};
+      return;
+    }
+    completed += 1;
+    if (completed === len) {
+      callback(err, true);
+      return;
+    }
+  };
+
+  for (i = 0; i < len; i += 1) {
+    iterator(array[i], complete);
+  }
+};
